6c9710b45d7c6aa465570cc98466f3c17271ebcb
codal-jatinp <jparate@codal.com>
2025-05-27 12:03:30 +0530

    Proper build process

---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const commonConfig = {
+  mode: 'production',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+};
+
+const cjsConfig = {
+  ...commonConfig,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.cjs.js',
+    library: {
+      type: 'commonjs2',
+    },
+    clean: true,
+  },
+  externals: {
+    react: 'commonjs2 react'
+  },
+};
+
+const esmConfig = {
+  ...commonConfig,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.esm.js',
+    library: {
+      type: 'module',
+    },
+  },
+  experiments: {
+    outputModule: true,
+  },
+  externals: {
+    react: 'module react'
+  },
+};
+
+const umdConfig = {
+  ...commonConfig,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.umd.js',
+    library: {
+      name: 'ReactReactiveVal',
+      type: 'umd',
+      umdNamedDefine: true,
+    },
+    globalObject: 'this',
+  },
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+    },
+  },
+};
+
+module.exports = [cjsConfig, esmConfig, umdConfig]; 
